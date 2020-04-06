26c05d2f73ab7361fa34a7547d9289163017c6f1
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2020-04-05 20:47:34 -0400

    Create stats-collection.sh

---
+++ Project/stats-collection.sh
@@ -0,0 +1,7 @@
+#Stats acquired from naturalstattrick.com
+#Player analytics:
+cat Hurricane-Players-Stats.csv | cut -d',' -f1,2,3,4,18,19 > Advanced-Player-Stats.csv
+
+#Games:
+
+#Free agents entered manually.
