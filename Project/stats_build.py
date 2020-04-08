3f347f269ab8fae480b896e6a749a0679a5b35ce
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2020-04-07 21:54:14 -0400

    Update stats_build.py

--- Project/stats_build.py
+++ Project/stats_build.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-basic_stats = pd.read_csv('Individual_Stats_Basic.csv', sep=',')
-                 #names=['Name', 'Position', 'GP', 'Goals', 'Total Assists', 'Total Points', 'PIM'])
-#print(basic_stats['Name', 'Position', 'GP', 'Goals', 'Total Assists', 'Total Points', 'PIM'])
-#basic_stats.columns = basic_stats.columns.to_string
-print(basic_stats.columns.str.decode('utf-8'))
+basic_stats = pd.read_csv('Individual_Stats_Basic.csv', index_col='Player', usecols=['Player', 'Position', 'GP', 'Goals', 'Total Assists', 'Total Points', 'PIM'])
+print(basic_stats)
+
