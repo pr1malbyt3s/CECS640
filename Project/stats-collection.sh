6c1de546f2e968e31a7ae145d056c46bfd41c8a4
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2020-04-12 19:36:22 -0400

    Delete stats-collection.sh

--- Project/stats-collection.sh
+++
@@ -1,7 +0,0 @@
-#Stats acquired from naturalstattrick.com
-#Player analytics:
-cat Hurricane-Players-Stats.csv | cut -d',' -f1,2,3,4,18,19 > Advanced-Player-Stats.csv
-
-#Games:
-
-#Free agents entered manually.
