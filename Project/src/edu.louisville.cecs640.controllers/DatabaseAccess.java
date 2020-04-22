2b19d9f80b22c6601f248fdcdb00099b243c0b32
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2020-04-22 08:53:10 -0400

    Update DatabaseAccess.java

--- Project/src/edu.louisville.cecs640.controllers/DatabaseAccess.java
+++ Project/src/edu.louisville.cecs640.controllers/DatabaseAccess.java
@@ -13,7 +13,12 @@ import edu.louisville.cecs640.final_project.Agent_Bean;
 import edu.louisville.cecs640.final_project.Game_Bean;
 import edu.louisville.cecs640.final_project.Player_Bean;
 
+/* Class used to control database access across application.
+ * Methods are called from respective servlets to perform specific functions.
+ */
+
 public class DatabaseAccess {
+	// Set global variables for class.
 	private Connection dbConnection = null;
 	private String dbName = null;
 	private String userID = null;
@@ -23,6 +28,10 @@ public class DatabaseAccess {
 	private String driver = null;
 	private String crypt = null;
 	
+	/* Method to get database connection properties from configuration file.
+	 * Prevents connection details, such as credentials from being hard-coded in source.
+	 * Assigns retrieved properties to global connection variables.
+	 */
 	public void GetConnectionProperties(String fileName) {
 		Properties p = null;
 		try {
@@ -41,7 +50,8 @@ public class DatabaseAccess {
 		}
 		
 	}
-
+	
+	// Method that performs driver loading and database connection.
 	public void DatabaseConnect() {
 		url = host + dbName;
 		try {
@@ -59,6 +69,7 @@ public class DatabaseAccess {
 		}
 	}
 	
+	// Method that performs database disconnect upon query execution.
 	public void DatabaseDisconnect() {
 		try {
 			dbConnection.close();
@@ -68,6 +79,10 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method that performs user validation.
+	 * Accepts username and password to perform query.
+	 * Compares password against encrypted password value stored in database.
+	 */
 	public boolean ValidateUser(String user, String password) throws SQLException {
 		boolean validate = false;
 		String query = "SELECT * FROM Users WHERE UPPER(NAME) = UPPER(?) AND DECRYPT_CHAR(PASSWORD, ?) = ? ";
@@ -87,6 +102,10 @@ public class DatabaseAccess {
 		return validate;
 	}
 	
+	/* Method that retrieves all contents from Players table.
+	 * Stores retrieved contents in an ArrayList of Player_Bean types.
+	 * Returns constructed ArrayList.
+	 */
 	public ArrayList<Player_Bean> ListPlayers() throws SQLException {
 		ArrayList<Player_Bean> players = new ArrayList<Player_Bean>();
 		String query = "SELECT * FROM Players";
@@ -115,6 +134,9 @@ public class DatabaseAccess {
 		return players;
 	}
 	
+	/* Method used to delete an individual player by name.
+	 * Accepts player's name as input to execute query.
+	 */
 	public void DeletePlayer(String playerName) throws SQLException {
 		String query = "DELETE FROM Players WHERE NAME = ? ";
 		PreparedStatement ps = dbConnection.prepareStatement(query);
@@ -127,29 +149,33 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method used to update an individual player by name.
+	 * Accepts player's name, the attribute to update, and its value to execute query.
+	 * Only used to update statistics and not player roster attributes.
+	 */
 	public void UpdatePlayer(String playerName, String attribute, double value) throws SQLException {
 		String stat = attribute;
 		String query = null;
 		switch (stat) {
-			case "GP":
+			case "GP": // Games Played
 				query = "UPDATE Players SET GP = ? WHERE NAME = ?";
 				break;
-			case "G":
+			case "G": // Goals
 				query = "UPDATE Players SET G = ? WHERE NAME = ?";
 				break;
-			case "A":
+			case "A": // Assists
 				query = "UPDATE Players SET A = ? WHERE NAME = ?";
 				break;
-			case "PTS":
+			case "PTS": // Points
 				query = "UPDATE Players SET PTS = ? WHERE NAME = ?";
 				break;
-			case "PIM":
+			case "PIM": // Penalty Minutes
 				query = "UPDATE Players SET PIM = ? WHERE NAME = ?";
 				break;
-			case "xGF":
+			case "xGF": // Expected Goals For
 				query = "UPDATE Players SET xGF = ? WHERE NAME = ?";
 				break;
-			case "xGA":
+			case "xGA": // Expected Goals Against
 				query = "UPDATE Players SET xGA = ? WHERE NAME = ?";
 				break;
 		}
@@ -167,6 +193,10 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method used to insert a new player.
+	 * Accepts player's name, number, age, and position.
+	 * All statistical values are defaulted to zero.
+	 */
 	public void InsertPlayer (String playerName, int num, int age, String pos) throws SQLException {
 		String query = "INSERT INTO Players values(?, ?, ?, ?, 0, 0, 0, 0, 0, 0, 0)";
 		PreparedStatement ps = dbConnection.prepareStatement(query);
@@ -182,6 +212,10 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method that retrieves all contents from Games table.
+	 * Stores retrieved contents in an ArrayList of Game_Bean types.
+	 * Returns constructed ArrayList.
+	 */
 	public ArrayList<Game_Bean> ListGames() throws SQLException {
 		ArrayList<Game_Bean> games = new ArrayList<Game_Bean>();
 		String query = "SELECT * FROM Games";
@@ -209,6 +243,9 @@ public class DatabaseAccess {
 		return games;
 	}
 	
+	/* Method used to delete and individual game by date.
+	 * Accepts games date as a string to execute query.
+	 */
 	public void DeleteGame(String date) throws SQLException {
 		String query = "DELETE FROM Games WHERE DATE = ? ";
 		PreparedStatement ps = dbConnection.prepareStatement(query);
@@ -221,32 +258,36 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method used to update game statistical values.
+	 * Accepts game's date, attribute to update, and its updated value.
+	 * Cannot be used to update opponent.
+	 */
 	public void UpdateGame(String date, String attribute, double value) throws SQLException {
 		String stat = attribute;
 		String query = null;
 		switch (stat) {
-			case "GF":
+			case "GF": // Goals For
 				query = "UPDATE Games SET GF = ? WHERE DATE = ?";
 				break;
-			case "GA":
+			case "GA": // Goals Against
 				query = "UPDATE Games SET GA = ? WHERE DATE = ?";
 				break;
-			case "SF":
+			case "SF": // Shots For
 				query = "UPDATE Games SET SF = ? WHERE DATE = ?";
 				break;
-			case "xGF":
+			case "xGF": // Expected Goals For
 				query = "UPDATE Games SET xGF = ? WHERE DATE = ?";
 				break;
-			case "xGA":
+			case "xGA": // Expected Goals Against
 				query = "UPDATE Games SET xGA = ? WHERE DATE = ?";
 				break;
-			case "HDSF":
+			case "HDSF": // High Danger Shots For
 				query = "UPDATE Games SET HDSF = ? WHERE DATE = ?";
 				break;
-			case "HDSA":
+			case "HDSA": // High Danger Shots Against
 				query = "UPDATE Games SET HDSA = ? WHERE DATE = ?";
 				break;
-			case "PDO":
+			case "PDO": // PDO Rating
 				query = "UPDATE Games SET PDO = ? WHERE DATE = ?";
 				break;
 		}
@@ -264,6 +305,10 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method used to insert new game into games table.
+	 * Accepts game date and opponent string values.
+	 * Statistical values defaulted to zero.
+	 */
 	public void InsertGame (String date, String opp) throws SQLException {
 		String query = "INSERT INTO Games values(?, ?, 0, 0, 0, 0, 0, 0, 0, 0)";
 		PreparedStatement ps = dbConnection.prepareStatement(query);
@@ -277,6 +322,10 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method that retrieves all contents from FreeAgents table.
+	 * Stores retrieved contents in an ArrayList of Agent_Bean types.
+	 * Returns constructed ArrayList.
+	 */
 	public ArrayList<Agent_Bean> ListAgents() throws SQLException {
 		ArrayList<Agent_Bean> agents = new ArrayList<Agent_Bean>();
 		String query = "SELECT * FROM FreeAgents";
@@ -303,6 +352,9 @@ public class DatabaseAccess {
 		return agents;
 	}
 	
+	/* Method used to delete free agent by name.
+	 * Accepts free agent's name to execute query.
+	 */
 	public void DeleteAgent(String playerName) throws SQLException {
 		String query = "DELETE FROM FreeAgents WHERE NAME = ? ";
 		PreparedStatement ps = dbConnection.prepareStatement(query);
@@ -315,23 +367,27 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method used to update free agent statistical values by name.
+	 * Accepts free agent's name, the attribute name, and updated attribute value.
+	 * Cannot be used to update free agent biological attributes.
+	 */
 	public void UpdateAgent(String playerName, String attribute, int value) throws SQLException {
 		String stat = attribute;
 		String query = null;
 		switch (stat) {
-			case "CAP":
+			case "CAP": // Expected Cap Hit
 				query = "UPDATE FreeAgents SET CAP = ? WHERE NAME = ? ";
 				break;
-			case "G":
+			case "G": // Goals
 				query = "UPDATE FreeAgents SET G = ? WHERE NAME = ? ";
 				break;
-			case "A":
+			case "A": // Assists
 				query = "UPDATE FreeAgents SET A = ? WHERE NAME = ? ";
 				break;
-			case "PTS":
+			case "PTS": // Points
 				query = "UPDATE FreeAgents SET PTS = ? WHERE NAME = ? ";
 				break;
-			case "PM":
+			case "PM": // Plus/Minus Rating
 				query = "UPDATE FreeAgents SET PM = ? WHERE NAME = ? ";
 				break;
 		}
@@ -349,6 +405,10 @@ public class DatabaseAccess {
 		}
 	}
 	
+	/* Method used to insert a free agent by name.
+	 * Accepts free agent's name, current team, age, and position.
+	 * All statistical values defaulted to zero.
+	 */
 	public void InsertAgent (String playerName, String teamName, int age, String pos) throws SQLException {
 		String query = "INSERT INTO FreeAgents values(?, ?, ?, ?, 0, 0, 0, 0, 0)";
 		PreparedStatement ps = dbConnection.prepareStatement(query);
